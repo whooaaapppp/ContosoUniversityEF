12959975efc0634677bb8cebc2b231280d9d9b91
whooaaapppp <jwarain@me.com>
2020-03-28 23:03:26 -0400

    update migration up func\

--- ContosoUniversity/Migrations/20200329030101_Inheritance.cs
+++ ContosoUniversity/Migrations/20200329030101_Inheritance.cs
@@ -8,93 +8,37 @@ namespace ContosoUniversity.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_CourseAssignment_Instructor_InstructorID",
-                table: "CourseAssignment");
+            name: "FK_Enrollment_Student_StudentID",
+            table: "Enrollment");
 
-            migrationBuilder.DropForeignKey(
-                name: "FK_Department_Instructor_InstructorID",
-                table: "Department");
+            migrationBuilder.DropIndex(name: "IX_Enrollment_StudentID", table: "Enrollment");
 
-            migrationBuilder.DropForeignKey(
-                name: "FK_Enrollment_Student_StudentID",
-                table: "Enrollment");
+            migrationBuilder.RenameTable(name: "Instructor", newName: "Person");
+            migrationBuilder.AddColumn<DateTime>(name: "EnrollmentDate", table: "Person", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "Discriminator", table: "Person", nullable: false, maxLength: 128, defaultValue: "Instructor");
+            migrationBuilder.AlterColumn<DateTime>(name: "HireDate", table: "Person", nullable: true);
+            migrationBuilder.AddColumn<int>(name: "OldId", table: "Person", nullable: true);
 
-            migrationBuilder.DropForeignKey(
-                name: "FK_OfficeAssignment_Instructor_InstructorID",
-                table: "OfficeAssignment");
+            // Copy existing Student data into new Person table.
+            migrationBuilder.Sql("INSERT INTO dbo.Person (LastName, FirstName, HireDate, EnrollmentDate, Discriminator, OldId) SELECT LastName, FirstName, null AS HireDate, EnrollmentDate, 'Student' AS Discriminator, ID AS OldId FROM dbo.Student");
+            // Fix up existing relationships to match new PK's.
+            migrationBuilder.Sql("UPDATE dbo.Enrollment SET StudentId = (SELECT ID FROM dbo.Person WHERE OldId = Enrollment.StudentId AND Discriminator = 'Student')");
+
+            // Remove temporary key
+            migrationBuilder.DropColumn(name: "OldID", table: "Person");
 
             migrationBuilder.DropTable(
-                name: "Instructor");
+                name: "Student");
 
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_Student",
-                table: "Student");
-
-            migrationBuilder.RenameTable(
-                name: "Student",
-                newName: "Person");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "FirstName",
-                table: "Person",
-                maxLength: 50,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(50)",
-                oldMaxLength: 50,
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "EnrollmentDate",
-                table: "Person",
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "datetime2");
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "HireDate",
-                table: "Person",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "Discriminator",
-                table: "Person",
-                nullable: false,
-                defaultValue: "");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_Person",
-                table: "Person",
-                column: "ID");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_CourseAssignment_Person_InstructorID",
-                table: "CourseAssignment",
-                column: "InstructorID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Cascade);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Department_Person_InstructorID",
-                table: "Department",
-                column: "InstructorID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.CreateIndex(
+                 name: "IX_Enrollment_StudentID",
+                 table: "Enrollment",
+                 column: "StudentID");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_Enrollment_Person_StudentID",
                 table: "Enrollment",
                 column: "StudentID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Cascade);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_OfficeAssignment_Person_InstructorID",
-                table: "OfficeAssignment",
-                column: "InstructorID",
                 principalTable: "Person",
                 principalColumn: "ID",
                 onDelete: ReferentialAction.Cascade);
